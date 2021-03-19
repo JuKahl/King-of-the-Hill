d60512133dd8dd6fe5c8e500ddf5dbd41abf9bed
JuKahl <julian.kahl@hotmail.de>
2021-03-19 15:36:56 +0100

    🔨 Add navigation/back button

---
+++ components/header/HeaderNavButton.tsx
@@ -0,0 +1,17 @@
+import styles from "./Header.module.css";
+
+export function NavMenuButton() {
+  return (
+    <button className={styles.menuBtn}>
+      <img src="icons/menu.svg" />
+    </button>
+  );
+}
+
+export function NavBackButton() {
+  return (
+    <button className={styles.backBtn}>
+      <img src="icons/back.svg" />
+    </button>
+  );
+}

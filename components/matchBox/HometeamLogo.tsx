b589366edab5d0f28637ff92d1ea689348e249e1
JuKahl <julian.kahl@hotmail.de>
2021-03-24 15:30:17 +0100

    🔨 Add hometeam logo

---
+++ components/matchBox/HometeamLogo.tsx
@@ -0,0 +1,9 @@
+import styles from "./MatchBox.module.css";
+
+export type Logo = {
+  logo: string;
+};
+
+export default function HometeamLogo({ logo }: Logo) {
+  return <img className={`${styles.hometeamLogo}`} src={logo} />;
+}

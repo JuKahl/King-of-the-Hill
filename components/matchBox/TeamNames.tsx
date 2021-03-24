f6ec17e8b0f1e43de9be84191f3462fe462ef303
JuKahl <julian.kahl@hotmail.de>
2021-03-24 15:35:27 +0100

    🔨 Add teamnames

---
+++ components/matchBox/TeamNames.tsx
@@ -0,0 +1,14 @@
+import styles from "./MatchBox.module.css";
+
+export type Teams = {
+  home: string;
+  away: string;
+};
+
+export default function Teamnames({ home, away }: Teams) {
+  return (
+    <p className={`${styles.teamnames}`}>
+      {home} vs. {away}
+    </p>
+  );
+}

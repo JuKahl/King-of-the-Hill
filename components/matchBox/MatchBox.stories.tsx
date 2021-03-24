9ac76c88bc601f4b4e23d57ff681bfcf2b5f4bd9
JuKahl <julian.kahl@hotmail.de>
2021-03-24 15:46:05 +0100

    Add storybook

---
+++ components/matchBox/MatchBox.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import MatchBox, { MatchBoxProps } from "./MatchBox";
+
+export default {
+  title: "Common/MatchBox",
+  component: MatchBox,
+} as Meta;
+
+const Template: Story<MatchBoxProps> = (args) => <MatchBox {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  // awayteamLogo: "https://crests.football-data.org/4.svg",
+  awayteam: "Dortmund",
+  // hometeamLogo: "https://crests.football-data.org/1.svg",
+  hometeam: "Köln",
+};

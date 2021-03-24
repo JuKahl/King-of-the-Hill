089cf680eb6cc097b9dc34ffd8bcf2a0733e723a
JuKahl <77615514+JuKahl@users.noreply.github.com>
2021-03-24 14:18:55 +0100

    Add Accordion component  (#18)
    * 🔨 Add accordion component
    
    * 💄 Add styling
    
    * ♻️  Refactor naming

---
+++ components/accordion/Accordion.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import styles from "./Accordion.module.css";
+
+export type AccordionProps = {
+  title: string;
+  content: string;
+};
+
+export default function Accordion({ title, content }: AccordionProps) {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className={styles.accordionItem}>
+      <div
+        className={styles.accordionTitle}
+        onClick={() => setIsActive(!isActive)}
+      >
+        <div>{title}</div>
+        <div>{isActive ? "-" : "+"}</div>
+      </div>
+      {isActive && <div className={styles.accordionContent}>{content}</div>}
+    </div>
+  );
+}

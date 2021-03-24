40d8b0c66eee8decf0cef9d6500d21c3b95cbe59
JuKahl <julian.kahl@hotmail.de>
2021-03-24 15:28:52 +0100

    🔨 Add awayteam logo

---
+++ components/matchBox/AwayteamLogo.tsx
@@ -0,0 +1,9 @@
+import styles from "./MatchBox.module.css";
+
+export type Logo = {
+  logo: string;
+};
+
+export default function AwayteamLogo({ logo }: Logo) {
+  return <img className={`${styles.awayteamLogo}`} src={logo} />;
+}

a731d2596a1c1116d893a63e7ea0262fa7441da4
JuKahl <77615514+JuKahl@users.noreply.github.com>
2021-03-29 10:50:14 +0200

    Add MongoDB connection (#22)
    * ➕ Install MongoDb
    
    * 🙈 Add .env
    
    * Add samples.http
    
    * 🗃 Add mongodb connection
    
    * Update naming & add 'POST' method
    
    * Add 'createUserDoc' in order to create user data

---
+++ server/db.ts
@@ -0,0 +1,41 @@
+import { MongoClient } from "mongodb";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+let client = null;
+let db = null;
+
+const url = process.env.MONGODB_URL;
+
+type Handler = (req: NextApiRequest, res: NextApiResponse) => void;
+export const withDatabase = (handler: Handler) => async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  await connectDB(url, "koth");
+  return await handler(req, res);
+};
+
+export async function connectDB(url, dbName) {
+  if (db) {
+    return;
+  }
+  client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  db = client.db(dbName);
+}
+
+export async function getCollection(collectioName) {
+  return await db.collection(collectioName);
+}
+
+export async function userList(collectioName) {
+  return await db.collection(collectioName).find().toArray();
+}
+
+export async function createUserDoc(userDoc) {
+  const orderCollection = await getCollection("users");
+  return await orderCollection.insertOne(userDoc);
+}
+
+export function closeDB() {
+  client.close();
+}

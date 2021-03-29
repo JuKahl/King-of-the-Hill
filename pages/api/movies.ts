cb4ca7beeebdaf6814dc570fa7236fd4e0e18b34
JuKahl <julian.kahl@hotmail.de>
2021-03-29 10:47:04 +0200

    Update naming & add 'POST' method

--- pages/api/movies.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { withDatabase, movieList } from "../../server/db";
-
-export default withDatabase(
-  async (req: NextApiRequest, res: NextApiResponse) => {
-    const list = await movieList("movies");
-    res.status(200).json(list);
-  }
-);

2e278e4c1f23e1a57d1d70d45e2518c896981ca9
JuKahl <julian.kahl@hotmail.de>
2021-03-31 21:54:37 +0200

    Add UserProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
 export default MyApp;

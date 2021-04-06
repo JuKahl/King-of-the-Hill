8778433d72861f39b5bce528426ca9b4f37c293d
JuKahl <77615514+JuKahl@users.noreply.github.com>
2021-04-06 18:54:42 +0200

    Update index page (#35)
    * 💄 Add global H1 font
    
    * 🍱  Add King of the Hill logo
    
    * 💄 Add styling
    
    * 💫  Add index page with automatic forwarding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,22 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Button from "../components/button/Button";
+import router from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    setTimeout(() => router.push("/login"), 3000);
+  });
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>King of the Hill</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Button primary label="Primary" />
-        <Button primary={false} label="Secondary" />
+      <main>
+        <h1 className={styles.title}>King of the Hill</h1>
+        <img className={styles.logo} src="/KingOfTheHill.png" alt="Logo" />
       </main>
     </div>
   );

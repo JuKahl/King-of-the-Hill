63c3410b501435f541eaedf3b4bce504be834629
JuKahl <julian.kahl@hotmail.de>
2021-04-06 14:10:47 +0200

    Add separate header with back button

---
+++ components/header/HeaderBackButton.tsx
@@ -0,0 +1,38 @@
+import styles from "./Header.module.css";
+import ProfilePicture from "./HeaderProfile";
+import SubNav from "./HeaderSubNav";
+import { NavBackButton } from "./HeaderNavButton";
+
+export type NavHeaderProps = {
+  label: string;
+  picture: string;
+  subNavOne: string;
+  subNavTwo: string;
+  linkOne: string;
+  linkTwo: string;
+};
+
+export default function NavHeader({
+  label,
+  picture,
+  subNavOne,
+  subNavTwo,
+  linkOne,
+  linkTwo,
+}: NavHeaderProps) {
+  return (
+    <div>
+      <section className={`${styles.navHeader}`}>
+        <NavBackButton />
+        <h1 className={`${styles.navHeaderLabel}`}>{label}</h1>
+        <ProfilePicture picture={picture} />
+      </section>
+      <SubNav
+        subNavOne={subNavOne}
+        subNavTwo={subNavTwo}
+        linkOne={linkOne}
+        linkTwo={linkTwo}
+      />
+    </div>
+  );
+}

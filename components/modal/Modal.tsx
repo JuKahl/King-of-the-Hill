ad5c1bbafe482dc84202293502dcb7e0ee154138
JuKahl <julian.kahl@hotmail.de>
2021-04-07 14:23:22 +0200

    Update Modal component

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -1,24 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { MouseEventHandler } from "react";
 import styles from "./1.module.css";
 
-const Modal = ({ visible, toggle, userPick }) =>
-  visible
-    ? ReactDOM.createPortal(
-        <div className={styles.modal}>
-          <div className={styles.modalPop} role="dialog" aria-modal={true}>
-            <p>Do you really want to pick {userPick} ?</p>
-            <button className={styles.buttonYes} onClick={toggle}>
-              Yes
-            </button>
-            <button className={styles.buttonNo} onClick={toggle}>
-              No
-            </button>
-          </div>
-          <div className={styles.modalOverlay}></div>
-        </div>,
-        document.body
-      )
-    : null;
+export type ModalProps = {
+  pick: any;
+  onAccept: MouseEventHandler;
+  onDecline: MouseEventHandler;
+};
+
+const Modal = ({ onDecline, onAccept, pick }: ModalProps) => (
+  <div className={styles.modal}>
+    <div className={styles.modalPop} role="dialog" aria-modal={true}>
+      <p>Do you really want to pick {pick.pick} ?</p>
+      <button className={styles.buttonYes} onClick={onAccept}>
+        Yes
+      </button>
+      <button className={styles.buttonNo} onClick={onDecline}>
+        No
+      </button>
+    </div>
+    <div className={styles.modalOverlay}></div>
+  </div>
+);
 
 export default Modal;

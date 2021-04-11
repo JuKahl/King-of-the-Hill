fe76153f5311a5d9aaeb1a54a0fa6d3b6add230f
JuKahl <77615514+JuKahl@users.noreply.github.com>
2021-04-11 14:51:29 +0200

    Update nav to highlight active link (#60)
    * 💄 Add styling for active & inactive link
    
    * Add function to highlight active link

--- components/header/HeaderSubNav.tsx
+++ components/header/HeaderSubNav.tsx
@@ -1,6 +1,7 @@
-import Link from "next/link";
 import React from "react";
 import styles from "./Header.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export type Navigation = {
   subNavOne: string;
@@ -15,14 +16,28 @@ export default function SubNav({
   linkOne,
   linkTwo,
 }: Navigation) {
+  const router = useRouter();
+
   return (
     <section className={styles.subNav}>
-      <Link href={linkOne}>
-        <button className={styles.navOne}>{subNavOne}</button>
-      </Link>
-      <Link href={linkTwo}>
-        <button className={styles.navTwo}>{subNavTwo}</button>
-      </Link>
+      <div
+        className={
+          router.pathname == `${linkOne}` ? styles.active : styles.inactive
+        }
+      >
+        <Link href={linkOne}>
+          <button className={styles.navOne}>{subNavOne}</button>
+        </Link>
+      </div>
+      <div
+        className={
+          router.pathname == `${linkTwo}` ? styles.active : styles.inactive
+        }
+      >
+        <Link href={linkTwo}>
+          <button className={styles.navTwo}>{subNavTwo}</button>
+        </Link>
+      </div>
     </section>
   );
 }
